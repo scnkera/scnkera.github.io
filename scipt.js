8d1c2fc0bfea257bafe137f0fe0d1308695f8c9a
scnkera <149810869+scnkera@users.noreply.github.com>
2024-06-21 14:45:45 -0400

    Updated html and css

---
+++ scipt.js
@@ -0,0 +1,9 @@
+// email alert ; temporary until email sending is activated
+const submission_button = document.getElementById("message_button");
+
+submission_button.onclick = () => {
+    console.log("click");
+    // alert("Email has been sent");
+}
+
+
