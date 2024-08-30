2af3fec1289d50d148a53160aba60163fc702c12
Christelle Nkera <scnkera@gmail.com>
2024-08-30 01:39:13 -0400

    Updated javascript, css, and static folder

--- script/scipt.js
+++ script/scipt.js
@@ -4,39 +4,45 @@ button.addEventListener("click", showMessage);
 function showMessage() {
     alert("Thanks for the message. I'll be in touch!");
 }
-
 document.addEventListener("DOMContentLoaded", function() {
     const navBar = document.getElementById("nav_bar");
     const links = navBar.getElementsByTagName("a");
+    let activeLink = links[0]; // Set the Home button as the initially active link
 
     // Set default colors: Home -> Blue, Others -> Gray
-    links[0].style.color = "#085ED6"; // Home button
+    activeLink.style.color = "blue"; // Home button
     for (let i = 1; i < links.length; i++) {
         links[i].style.color = "gray"; // Other buttons
+    }
+
+    // Function to reset all links to gray except the active one
+    function resetColors() {
+        for (let j = 0; j < links.length; j++) {
+            if (links[j] !== activeLink) {
+                links[j].style.color = "gray";
+            }
+        }
     }
 
     // Add event listeners for hover and click events
     for (let i = 0; i < links.length; i++) {
         links[i].addEventListener("mouseover", function() {
-            links[i].style.color = "#085ED6";
+            if (links[i] !== activeLink) {
+                links[i].style.color = "#085ED6";
+            }
         });
 
         links[i].addEventListener("mouseout", function() {
-            // If the link has not been clicked, revert to gray
-            if (!links[i].classList.contains("clicked")) {
+            if (links[i] !== activeLink) {
                 links[i].style.color = "gray";
             }
         });
 
         links[i].addEventListener("click", function() {
-            // Remove the 'clicked' class from all links
-            for (let j = 0; j < links.length; j++) {
-                links[j].classList.remove("clicked");
-                links[j].style.color = "gray"; // Reset all links to gray
-            }
-            // Add the 'clicked' class to the clicked link and set it to blue
-            links[i].classList.add("clicked");
-            links[i].style.color = "#085ED6";
+            // Update activeLink to the clicked one and reset other colors
+            activeLink = links[i];
+            resetColors();
+            activeLink.style.color = "#085ED6";
         });
     }
 });
